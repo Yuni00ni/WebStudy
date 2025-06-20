eac5737d34e39349c9df948c3d7e742d9d2a8ac4
uni <moonrit001@gmail.com>
2025-06-20 11:08:54 +0900

    가위바위보 완

--- JS/2. 심화/JS/ex02. 가위바위보.js
+++ JS/2. 심화/JS/ex02. 가위바위보.js
@@ -12,16 +12,33 @@ console.log(myChoice);
 //      - 랜덤게임에서 사용했던 랜덤한 수 뽑기를 이용 (ranNum)
 //      - 0,1,2 중 랜덤한 숫자를 뽑아서 그걸 인덱스 번호로 활용
 //      - 가위, 바위, 보 가 랜덤하게 추출 -> comChoice
-let comChoice = parseInt(Math.random()*3);
+let comChoiceIndex = parseInt(Math.random()*3);
+let comChoice = arr[comChoiceIndex];
+console.log(comChoiceIndex);
 console.log(comChoice);
 
 // 4. 문서에 (docume)nt.write) 나의 선택, 컴퓨터의 선택을 입력
 document.write('나의 선택 : ', myChoice);
 document.write('<br>')
-document.write('컴퓨터의 선택 : ', arr[comChoice]);
+document.write('컴퓨터의 선택 : ', comChoice);
+document.write('<br>')
 // 5. 가위바위보의 결과 또한 문서에 출력 (result - 승리, 패배, 무승부, 게임오류)
 //      - 가위바위보의 로직대로 승부 판정
 //      - 사용자가 다른 걸 입력했을 때 '게임오류'
-if(myChoice == arr[comChoice]) {
+if(myChoice == comChoice) {
     document.write('무승부');
+}else if(
+    (myChoice == "가위" && comChoice == "바위"),
+    (myChoice == "바위" && comChoice == "보"),
+    (myChoice == "보" && comChoice == "가위")
+) {
+    document.write("패배");
+}else if(
+    (myChoice == "가위" && comChoice == "보"),
+    (myChoice == "바위" && comChoice == "가위"),
+    (myChoice == "보" && comChoice == "바위")
+) {
+    document.write("승리");
+}else {
+    document.write("게임오류");
 }
