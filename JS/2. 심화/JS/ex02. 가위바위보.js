108d25dac6d28039ec5d123f5766dd3f7f60a6b1
uni <moonrit001@gmail.com>
2025-06-20 10:44:02 +0900

    가위바위보 진행중

---
+++ JS/2. 심화/JS/ex02. 가위바위보.js
@@ -0,0 +1,27 @@
+// 지금부터 가위바위보 게임을 만들어보겠습니다.
+// 1. arr 라는 배열 안에 '가위', '바위', '보' 라는 데이터를 넣습니다.
+let arr = ["가위", "바위", "보"];
+
+// 2. 사용자의 선택을 입력받습니다. -> myChoice
+//      - 무엇을 내시겠습니까? (가위, 바위, 보)
+let myChoice = prompt("무엇을 내시겠습니까?");
+
+console.log(myChoice);
+
+// 3. 컴퓨터의 선택을 세팅합니다. -> comChoice
+//      - 랜덤게임에서 사용했던 랜덤한 수 뽑기를 이용 (ranNum)
+//      - 0,1,2 중 랜덤한 숫자를 뽑아서 그걸 인덱스 번호로 활용
+//      - 가위, 바위, 보 가 랜덤하게 추출 -> comChoice
+let comChoice = parseInt(Math.random()*3);
+console.log(comChoice);
+
+// 4. 문서에 (docume)nt.write) 나의 선택, 컴퓨터의 선택을 입력
+document.write('나의 선택 : ', myChoice);
+document.write('<br>')
+document.write('컴퓨터의 선택 : ', arr[comChoice]);
+// 5. 가위바위보의 결과 또한 문서에 출력 (result - 승리, 패배, 무승부, 게임오류)
+//      - 가위바위보의 로직대로 승부 판정
+//      - 사용자가 다른 걸 입력했을 때 '게임오류'
+if(myChoice == arr[comChoice]) {
+    document.write('무승부');
+}
