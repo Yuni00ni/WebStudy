d096719435f742dbfdb930da7d1a1efe9444882b
uni <moonrit001@gmail.com>
2025-06-24 17:56:52 +0900

    하다 말았음

---
+++ JS/2. 심화/JS/ex12. 영화데이터 실습.js
@@ -0,0 +1,27 @@
+// 표 형식으로 영화 순위, 영화 이름, 개봉일 th로 테이블 나열하기
+// req_btn onclick 표 생성
+
+// movie url
+let movieURL = 'https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=0170eee9fa98a2d950aaa0575e7d94d1&targetDt=20250623';
+
+// moviedata res & req
+const getMovieData = async () => {
+    // 서버로부터 응답이 올 때까지 잠시 대기! -> await
+    let res = await fetch(movieURL);
+    let data = await res.json();
+    console.log('영화 정보 : ', data);
+    
+    // 데이터 표 만들기 순위 : rank, 이름 : movieNm, 개봉일 : openDt
+    let movieDataList = data.boxOfficeResult.dailyBoxOfficeList;
+    console.log(movieDataList);
+    for(let i = 0; i < movieDataList.length; i++) {
+        let movieList = document.getElementById('movie-list');
+        movieList.innerText = `<table><th>순위</th><th>영화 이름</th><th>개봉일</th><tbody><td>${movieList[i].rank}</td><td>${movieList[i].movieNm}<td>${}</td></td></tbody></table>`
+    }
+    
+}
+// onclick event
+const req_btn = document.getElementById('req_btn');
+req_btn.addEventListener('click', getMovieData);
+
+
