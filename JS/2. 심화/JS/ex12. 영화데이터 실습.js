55d837c2a4190e0bb019f08ddf1486cc0e76dc3b
uni <moonrit001@gmail.com>
2025-06-25 10:52:24 +0900

    ex12. 영화데이터 실습 주석 추가까지

--- JS/2. 심화/JS/ex12. 영화데이터 실습.js
+++ JS/2. 심화/JS/ex12. 영화데이터 실습.js
@@ -12,24 +12,22 @@ const getMovieData = async () => {
     console.log('영화 정보 : ', data);
     
     // 데이터 표 만들기 순위 : rank, 이름 : movieNm, 개봉일 : openDt
-    let movieDataList = data.boxOfficeResult.dailyBoxOfficeList;
-    console.log(movieDataList);
+    let movieDataList = data.boxOfficeResult.dailyBoxOfficeList; // 객체 내부 배열까지 접근해서 변수에 담기
+    console.log(movieDataList); // 디버깅
     
-    let movieList = document.getElementById('movie-list');
-    movieList.innerHTML = '<table border="1px solid black"> <th>순위</th><th>영화 이름</th><th>개봉일</th><tbody></tbody></table>'
+    let movieList = document.getElementById('movie-list'); // html 내부 태그 id값으로 가져오기
+    movieList.innerHTML = '<table border="1px solid black"> <th>순위</th><th>영화 이름</th><th>개봉일</th><tbody></tbody></table>' // innerhtml 이용해서 내부에 표 기본 틀 작성하기
     
-    let tbody = document.querySelector('tbody');
+    // ** innerhtml로 작성한 태그도 가져올 수 있음! 코드 진행 순서만 잘 맞춰주면 잘 동작함!
+    let tbody = document.querySelector('tbody'); // 선택자 이용해서 방금 작성했던 movie-list 내부의 태그 중 tbody를 가져오기
     
     for(let i = 0; i < movieDataList.length; i++) {
-        tbody.innerHTML += `<td>${movieDataList[i].rank}</td><td>${movieDataList[i].movieNm}</td><td>${movieDataList[i].openDt}</td>`
+        tbody.innerHTML += `<td>${movieDataList[i].rank}</td><td>${movieDataList[i].movieNm}</td><td>${movieDataList[i].openDt}</td>` // innerhtml 이용해서 tbody 내부에 들어갈 내용 반복문으로 작성하기
     }
-
-    
-
-    // '<table border="1px solid black"><th>순위</th><th>영화 이름</th><th>개봉일</th><tbody><td>' + movieList[i].rank + '</td><td>' + movieList[i].movieNm + '<td>'+ movieList[i].openDt + '</td></td></tbody></table>'
 }
+
 // onclick event
-const req_btn = document.getElementById('req_btn');
-req_btn.addEventListener('click', getMovieData);
+const req_btn = document.getElementById('req_btn'); // html 내부 태그 id값으로 가져오기
+req_btn.addEventListener('click', getMovieData); // 태그 클릭 시 이벤트 추가
 
 
