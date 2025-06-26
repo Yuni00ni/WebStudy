0c5f319540ab6a64388da9a7524e81c04acb77bd
uni <moonrit001@gmail.com>
2025-06-26 16:01:25 +0900

    콜백함수 설명까지

---
+++ node.js/ex01. node/ex02. http.js
@@ -0,0 +1,7 @@
+// ctrl + ,
+const http = require('http'); // node.js Core API 모듈 중 하나인 http 모듈 사용하겠다!(서버 생성, 응답 정의 ....)
+
+// http 모듈 사용해서 서버 열기
+// 새 http 서버 인스턴스(객체-도구) 만듦
+// 매개변수 : 콜백함수(createServer함수 실행 후 마무리가 되면 그 결과값을 가지고 호출할 함수)
+http.createServer(() => {});
