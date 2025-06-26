5354e82e19d6dc1ff3522444caed6a39e1e456cc
uni <moonrit001@gmail.com>
2025-06-26 17:35:45 +0900

    복습.txt 추가

--- node.js/ex01. node/ex04. url.js
+++ node.js/ex01. node/ex04. url.js
@@ -1,7 +1,19 @@
 const http = require('http');
 
+// url 모듈
+const url = require('url');
+
 http.createServer((req, res) => {
-    
+
+        const parseUrl = url.parse(req.url, true);
+        console.log(parseUrl);
+        
+        // url => url(주소 : localhost:8888) + query parameter
+        console.log('pathname : ', parseUrl.pathname); // 요청 기본 URL
+        console.log('query : ', parseUrl.query); // query 빼오기
+        
+        
+
 }).listen(8888, () => {
     console.log('Server is running on http://localhost:8888');
 });
