22e49e5fd47b35ecab16cae9141f244d90000081
uni <moonrit001@gmail.com>
2025-07-01 10:32:32 +0900

    mysql2 모듈 설치

--- node.js/ex03. DB/app.js
+++ node.js/ex03. DB/app.js
@@ -1,24 +1,26 @@
 const express = require('express');
 const path = require('path'); // 경로 다루는 모듈
-
+const pageRouter = require('./routes/pageRouter');
 
 const app = express(); // app생성
-// 주소창에 주소를 입력해서 요청하는 것은 -> get요청
-// 단순히 페이지만 보여지는 것 -> get
-// join.html 응답 localhost:8888/join
-app.get('/join', (req, res)=>{
-  // __dirname : 현재 파일(app.ja)이 있는 절대 경로(ex03. DB폴더 내부) => ex03. DB경로/
-  res.sendFile(path.join(__dirname, 'views', 'join.html'));
-});
 
-// login.html 응답 localhost:8888/login
-app.get('/login',(req, res)=> {
-  res.sendFile(path.join(__dirname, 'views', 'login.html'));
+// body-parser
+app.use(express.urlencoded({extended:true})); // 폼 데이터 처리
+
+// localhost:3000/~ => pageRouter
+app.use('/', pageRouter);
+
+app.post('/user/join', (req, res)=>{
+  // 요청데이터 가져오기(body)
+  console.log(req.body);
+  const {id, pw, nick} = req.body;
+
+  
 })
 
-app.listen(8888, ()=>{
-  console.log('Server is running on http://localhost:8888');
-  console.log('join page http://localhost:8888/join');
-  console.log('login page http://localhost:8888/login');
+app.listen(3000, ()=>{
+  console.log('Server is running on http://localhost:3000');
+  console.log('join page http://localhost:3000/join');
+  console.log('login page http://localhost:3000/login');
 });
 
--- node.js/ex03. DB/routes/pageRouter.js
+++ node.js/ex03. DB/routes/pageRouter.js
@@ -1,2 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+
+// 주소창에 주소를 입력해서 요청하는 것은 -> get요청
+// 단순히 페이지만 보여지는 것 -> get
+// join.html 응답 localhost:3000/join
+router.get('/join', (req, res) => {
+    // __dirname : 현재 파일(pageRouter.js)이 있는 절대 경로(routes) => routes 경로/
+    res.sendFile(path.join(__dirname, '..', 'views', 'join.html'));
+});
+
+// login.html 응답 localhost:3000/login
+router.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'views', 'login.html'));
+});
+
+module.exports = router;
