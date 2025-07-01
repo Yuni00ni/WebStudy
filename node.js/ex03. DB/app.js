a0aa357f3ec6f4edc78f34e6c4815363c488df10
uni <moonrit001@gmail.com>
2025-07-01 11:57:49 +0900

    login까지

--- node.js/ex03. DB/app.js
+++ node.js/ex03. DB/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path'); // 경로 다루는 모듈
 const pageRouter = require('./routes/pageRouter');
-const {createMember} = require('./models/memberModel');
+const {createMember, loginMember} = require('./models/memberModel');
 
 const app = express(); // app생성
 
@@ -11,13 +11,32 @@ app.use(express.urlencoded({extended:true})); // 폼 데이터 처리
 // localhost:3000/~ => pageRouter
 app.use('/', pageRouter);
 
-app.post('/user/join', (req, res)=>{
+app.post('/user/join', async (req, res)=>{
   // 요청데이터 가져오기(body)
   console.log(req.body);
   const {id, pw, nick} = req.body;
-  createMember(id, pw, nick);
+  const result = await createMember(id, pw, nick); // 1 -> 회원가입 성공 / 0 -> 회원가입 실패
   
-})
+  console.log('result : ', result);
+  
+  if(result>0) { // 1
+    // 최초로 요청한 경로(/user/join)가 주소창에 그대로 남음
+    // res.sendFile(path.join(__dirname, 'views', 'login.html'));
+    // redirect 방식 : 클라이언트에게 특정 주소로 다시 요청하라고 응답하는 방식
+    res.redirect('/login');
+  }else { // 0
+    // res.sendFile(path.join(__dirname, 'views', 'join.html'));
+    res.redirect('/join');
+
+  }
+});
+
+// 로그인 : 사용자가 입력한 아이디, 비밀번호가 member DB 있는 지 확인하고 있으면(성공) 없으면(실패)
+app.post('/user/login', (req, res)=> {
+  const {id, pw} = req.body;
+  loginMember(id, pw);
+
+});
 
 app.listen(3000, ()=>{
   console.log('Server is running on http://localhost:3000');
--- node.js/ex03. DB/models/memberModel.js
+++ node.js/ex03. DB/models/memberModel.js
@@ -5,17 +5,35 @@ async function createMember(id, pw, nick){
     // 1. Connect
     const conn = await pool.getConnection();
 
-    // 2. SQL 실행
-    const result = await conn.execute('insert into member(id, pw, nick) values(?, ?, ?)',
-        [id, pw, nick]
-    );
 
-    console.log(result);
-    
-    // 3. 결과 처리
+    try{ // 무조건 실행해야 하는 블록 ~> 중간에 오류가 발생할 경우 catch블록에서 처리
+        // 2. SQL 실행
+        // [result] => execute 결과값은 배열, 배열의 첫 번째 값이 result 변수에 들어감
+        const [result] = await conn.execute('insert into member(id, pw, nick) values(?, ?, ?)',
+            [id, pw, nick]
+        );
+        
+        // 3. 결과 처리
+        return result.affectedRows;
+    }finally { // try든 catch든 마무리 후에는 무조건 마지막으로 실행하는 블록
+        // 4. 연결 해제
+        conn.release();
+    }
 
-    // 4. 연결 해제
-    conn.release();
 };
 
-module.exports = {createMember};
+// 로그인
+async function loginMember(id, pw){
+    const conn = await pool.getConnection();
+
+    try {
+        // READ => select
+        const result = await conn.execute('select * from member where id = ? and pw = ?',
+        [id, pw]);
+        console.log(result);
+    }finally {
+        conn.release();
+    }
+}
+
+module.exports = {createMember, loginMember};
