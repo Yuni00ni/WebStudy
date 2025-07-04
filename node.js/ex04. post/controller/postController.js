95d205c4d0ff2b7bc4cb2fd052f07e543e24b6c6
uni <moonrit001@gmail.com>
2025-07-04 12:20:17 +0900

    주석 추가

--- node.js/ex04. post/controller/postController.js
+++ node.js/ex04. post/controller/postController.js
@@ -3,6 +3,7 @@ const { create } = require('../models/postModel');
 
 async function createPost(req, res) {
     const post = req.body; // json(title, writer, content)
+    //{title: '제목', writer:'작성자'...}
     const filename = req.file.filename; // 업로드한 이미지 파일 이름
 
     // 파일 이름까지 post json 객체에 추가
