461411e17f14645eee8d845160641d4429711026
uni <moonrit001@gmail.com>
2025-07-09 10:49:54 +0900

    Ex04 map함수 사용해보기 시작

--- React/03.usestate/src/example/Ex03.jsx
+++ React/03.usestate/src/example/Ex03.jsx
@@ -12,19 +12,20 @@ const Ex03 = () => {
 
     // 함수 선언부
     // 버튼 클릭 시, 버튼에 적용되어있는 이벤트를 확인(이벤트 객체)
-    const btnCk = (event) => {
+    // 이벤트 객체 사용 시 => event, e 둘 중에 편한 키워드를 선택
+    const btnCk = (e) => {
+
         // console.log('버튼 이벤트');
-        console.log(event.target.innerText);
+        // console.log(event.target.innerText);
+
         // 클릭한 버튼의 숫자를 inputNum이라는 state안에 넣어주기
         // 형 변환(parseInt())
-        setInputNum(parseInt(event.target.innerText))
+        setInputNum(parseInt(e.target.innerText))
 
         // 랜덤 수 만들기 -> ranNum이라는 state안에 넣어주기
-        parseInt(Math.random() * 3)
-        console.log(parseInt(Math.random() * 3 + 1));
         setRanNum(parseInt(Math.random() * 3 + 1))
-        
-        
+
+
 
     }
 
@@ -41,10 +42,12 @@ const Ex03 = () => {
                 <br />
                 랜덤한 숫자 : {ranNum}
                 <br />
+
                 {/* 조건부 랜더링으로 처리 => 조건 2개(삼항연산자) */}
                 {/* 비교했을 때, 두 개의 값이 같다면(true) '정답입니다' */}
                 {/* 두 개의 값이 같지 않다면(false) '오답입니다' */}
-                {inputNum === ranNum ? '정답입니다' : '오답입니다'}
+                {inputNum === ranNum ? '정답입니다~' : '오답입니다~'}
+
             </div>
         </div>
     )
---
+++ React/03.usestate/src/example/Ex04.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Ex04 = () => {
+    // 배열 안에 객체데이터를 넣어서 출력 => 반복된 데이터로 출력
+    let person = [
+        {name : 'A', age : 1, bloodType : 'A'},
+        {name : 'B', age : 2, bloodType : 'B'},
+        {name : 'C', age : 3, bloodType : 'AB'}
+    ]
+    console.log(person);
+    
+  return (
+    <div></div>
+  )
+}
+
+export default Ex04
