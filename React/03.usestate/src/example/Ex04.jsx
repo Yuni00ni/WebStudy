2bc9e734aea2780e72bbed91d92381b84c7140c3
uni <moonrit001@gmail.com>
2025-07-09 11:46:15 +0900

    map함수 사용해보기 완

--- React/03.usestate/src/example/Ex04.jsx
+++ React/03.usestate/src/example/Ex04.jsx
@@ -7,7 +7,9 @@ const Ex04 = () => {
         { name: 'B', age: 2, bloodType: 'B' },
         { name: 'C', age: 3, bloodType: 'AB' }
     ]
-    console.log(person[0].name);
+
+    let food = ['라면', '도시락', '샐러드']
+    let numlist = [2,4,6,8,10]
 
     return (
         <div>
@@ -15,9 +17,29 @@ const Ex04 = () => {
             {/* 3명의 인물을 소개해주세요 => 배열안에 있는 객체 데이터를 꺼내
                 이름과 그리고 나이, 혈액형 => 소개하는 글을 작성
                 */}
-                <p>이름 : {person[0].name}, 나이 : {person[0].age}, 혈액형 : {person[0].bloodType}</p>
-                <p>이름 : {person[1].name}, 나이 : {person[1].age}, 혈액형 : {person[1].bloodType}</p>
-                <p>이름 : {person[2].name}, 나이 : {person[2].age}, 혈액형 :  {person[2].bloodType}</p>
+            <p>이름 : {person[0].name}, 나이 : {person[0].age}, 혈액형 : {person[0].bloodType}</p>
+            <p>이름 : {person[1].name}, 나이 : {person[1].age}, 혈액형 : {person[1].bloodType}</p>
+            <p>이름 : {person[2].name}, 나이 : {person[2].age}, 혈액형 :  {person[2].bloodType}</p>
+
+            <h3>Case 2 : Map 함수를 이용</h3>
+            {/* map함수는 기존의 배열안에 있는 데이터를 가져와서 새로운 배열로 반환함 => 기존의 배열은 영향을 받지 않는다 
+                    => 기존 배열안에 있는 데이터를 -> item이라는 키워드로 묶어준다
+                    */}
+            {person.map((item) => (
+                <p key={item.name}>
+                    이름 : {item.name}, 나이 : {item.age}, 혈액형 : {item.bloodType}
+                </p>
+            ))}
+
+            <h3>Case 3 : 배열데이터를 변경하기</h3>
+            {/* map함수를 통해서 기존의 배열데이터를 => 버튼으로 변경 */}
+            {food.map((item) => (
+                <button key={item} style={ {backgroundColor : 'pink'}}>{item}</button>
+            ))}
+            <br />
+            {numlist.map((item) => (
+                <button key={item} style={{backgroundColor : 'yellowgreen'}}>{item}</button>
+            ))}
         </div>
     )
 }
