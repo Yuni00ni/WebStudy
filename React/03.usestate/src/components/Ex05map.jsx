f9e75d89e276505b182aacf1c5a292b3262dc117
uni <moonrit001@gmail.com>
2025-07-10 09:35:16 +0900

    bootstrap 이용해서 페이지 만들기(?)

--- React/03.usestate/src/components/Ex05map.jsx
+++ React/03.usestate/src/components/Ex05map.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
+// npm install react-bootstrap bootstrap => bootstrap사용시 필요한 모듈들 불러오기
+import 'bootstrap/dist/css/bootstrap.min.css'; // bootstrap css를 적용하는 코드
+import { Card, Button } from 'react-bootstrap'
 
-const Ex05map = ({item}) => {
-    console.log(item);
-    
+const Ex05map = ({ item }) => {
+  console.log(item);
+
   return (
-    <div>
-
-    </div>
+    <Card style={{ width: '18rem'}}>
+      <Card.Img variant="top" src={item.imgSrc} height= '350px' />
+      <Card.Body>
+        <Card.Title>{item.title} {item.birthYear}</Card.Title>
+        <Card.Text>
+          {item.content}
+        </Card.Text>
+        <a href={item.SNS}><Button variant="warning">Instagram</Button></a>
+      </Card.Body>
+    </Card>
   )
 }
 
--- React/03.usestate/src/example/Ex05.jsx
+++ React/03.usestate/src/example/Ex05.jsx
@@ -6,7 +6,8 @@ const Ex05 = () => {
     return (
         <div>
             <h1>Map 실습</h1>
-            <div>
+            {/* flex => 부모 요소에서 적용, 요소들을 가로로 배치 시켜줌 */}
+            <div style={{display : 'flex'}}>
                 {/* 우리가 가지고온 json파일을 활용해서 하위 컴포넌트로 값을 전달하기 */}
                 {data.result.map((item) => (
                     <Ex05map key={item.title} item={item}></Ex05map>
