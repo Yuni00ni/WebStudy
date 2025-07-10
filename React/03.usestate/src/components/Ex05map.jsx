9e0908b877454d119f5ca5a8f520d9db2321afdd
uni <moonrit001@gmail.com>
2025-07-10 09:13:34 +0900

    npm install react-bootstrap bootstrap

--- React/03.usestate/src/components/Ex05map.jsx
+++ React/03.usestate/src/components/Ex05map.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
 
 const Ex05map = ({item}) => {
+    console.log(item);
+    
   return (
     <div>
-            <p>이름 : {item.title}</p>
-            <p>출생년도 : {item.birthYear}</p>
-            <p>소개 : {item.content}</p>
-            <img src={item.imgSrc} alt="" />
-            <br />
-            <a href={item.SNS}>{item.SNS}</a>
+
     </div>
   )
 }
