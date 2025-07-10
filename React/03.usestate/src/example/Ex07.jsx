dd1a367a2f7b95a1cd82849ffdfcfd80e61d2d14
uni <moonrit001@gmail.com>
2025-07-10 12:28:45 +0900

    useRef 실습 완

--- React/03.usestate/src/example/Ex07.jsx
+++ React/03.usestate/src/example/Ex07.jsx
@@ -9,11 +9,12 @@ const Ex07 = () => {
 const divRef = useRef()
 
 // 수치를 받아줄 함수를 생성
-const changeSize = (n) => {
+const changeSize = (n, m) => {
     // 전달받은 매개변수(수치)를 통해서
     // 실제 div태그의 style속성으로 접근해서 높이와 너비를 변경
     divRef.current.style.height = n
     divRef.current.style.width = n
+    divRef.current.style.backgroundColor = m
 
 
 }
@@ -22,18 +23,18 @@ const ckBtn = (event) => {
     console.log(event);
     // 각 버튼을 클릭했을 때 버튼안에 있는 텍스트를 가져오기
     console.log(event.target.innerText);
-    console.log(divRef.current.style.height);
+    console.log(divRef.current.style.backgroundColor);
 
     // 조건문 생성 => 내가 클릭한 버튼안에 있는 텍스트가 작게라면 => div의 높이, 너비를 100px로 변경
     // 버튼안의 텍스트가 원본이라면 => 높이, 너비 200px로 변경
     // 위의 조건 모두 다 아니라면 300px로 변경
     if(event.target.innerText == '작게') {
-        changeSize('100px')
+        changeSize('100px', 'yellow')
         
     }else if(event.target.innerText == '원본') {
-        changeSize('200px')
+        changeSize('200px', 'skyblue')
     }else {
-        changeSize('300px')
+        changeSize('300px', 'green')
     }
 }
 
---
+++ React/03.usestate/src/example/Ex08.jsx
@@ -0,0 +1,32 @@
+import React, { useRef } from 'react'
+import '../style/ex08_input.css'
+
+const Ex08 = () => {
+
+    // useRef 사용
+     const inputRef = useRef()
+
+    // 함수 선언부
+    const buttonClick = () => {
+        // input태그안에서 값을 접근하는 속성 value
+        console.log(inputRef.current.value);
+        // 출력 => alert를 통해서 화면에 띄우기
+        alert(inputRef.current.value)
+        // 입력 공간 초기화
+        inputRef.current.value = ""
+        
+    }
+
+
+    return (
+        <div className='container'>
+            <header className='header'>
+                <h1>useRef Example</h1>
+                <input ref={inputRef} type="text" placeholder='Enter Something' />
+                <button onClick={buttonClick}>Show Input</button>
+            </header>
+        </div>
+    )
+}
+
+export default Ex08
