83d49533b78e44e825e2d5cdab77c429bb7a2964
uni <moonrit001@gmail.com>
2025-07-11 11:47:06 +0900

    컨텍스트 완

--- React/03.usestate/src/components/Ex10/ColorList.jsx
+++ React/03.usestate/src/components/Ex10/ColorList.jsx
@@ -7,12 +7,16 @@ const ColorList = () => {
     const { setChoicColor } = useContext(ColorContext)
 
     let color = ['red', 'orange', 'yellow', 'green', 'blue']
-
+    const ck = (e) => {
+        // 이벤트 객체를 통해서 div안에 색상 데이터를 가지고 오고, 가지고온 데이터를 set함수에 안에 넣어주기
+        console.log('ck : ', e.target.style.backgroundColor);
+        setChoicColor(e.target.style.backgroundColor)
+    }
     return (
-        <div>
+        <div style={{display : 'flex'}}>
             {/* map함수를 통해서 color변수 안에 있는 색상을 화면에 출력하기  높이 : '100px', 너비 : '100px' => div태그 5개*/}
-            {color.map((item) => (
-                <div key= {item} style={{width: '100px', height : '100px', backgroundColor : item, margin : '3px'}}></div>
+            {color.map((item) => ( 
+                <div onClick={ck} key= {item} style={{width: '100px', height : '100px', backgroundColor : item, margin : '3px', cursor : 'pointer'}}></div>
             ))}
 
         </div>
--- React/03.usestate/src/components/Ex10/ColorResult.jsx
+++ React/03.usestate/src/components/Ex10/ColorResult.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { ColorContext } from '../../context/Ex10ColorContext'
 
 const ColorResult = () => {
+    // 내가 만든 context를 사용하겠다
+    const { choiceColor } = useContext(ColorContext)
+    console.log('cC : ', choiceColor);
+    
     return (
-        <div>ColorResult</div>
+        <div>
+            <div style={{width : '100px', height : '100px', backgroundColor : choiceColor}}>
+                {/* 내가 클릭한 색상이 => state 안에 있음 
+                    너비, 높이 100px 배경색 지정
+                */}
+            </div>
+        </div>
     )
 }
 
