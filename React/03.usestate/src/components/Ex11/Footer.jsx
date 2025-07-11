15b5aa8aee6087c6955fe68a0f52711776d165d4
uni <moonrit001@gmail.com>
2025-07-11 13:54:19 +0900

    커밋

--- React/03.usestate/src/components/Ex11/Footer.jsx
+++ React/03.usestate/src/components/Ex11/Footer.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ThemeContext } from '../../context/Ex11ThemeContext'
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
+    // 컨텍스트 사용 => value 속성 안에 있는 2개의 값 가져오기
+    const { isDark, setIsDark } = useContext(ThemeContext)
+
+    // 함수명 => toggleTheme
+    // 사용목적은 state안에 있는 값을 계속 반대로 바꾸기 위해서
+    const toggleTheme = (e) => {
+        setIsDark(!isDark)
+        console.log(isDark);
+        console.log(e);
+        
+        
+    }
+    return (
+        <div>
+            <footer
+                onClick={toggleTheme}
+                className='footer'
+                style={{ backgroundColor : 'lightgray'
+                    // 배경색상 => 값을 삼항연산자로 처리
+                    // state값이 참이라면 black 거짓이라면 lightgray
+                }}
+            >
+                <button className='button' onClick={toggleTheme}>
+                    Dark Mode
+                    {/* state값이 참이라면 버튼 안 텍스트를 Lishgt Mode 거짓이라면 Dark Mode */}
+                </button>
+            </footer>
+        </div>
+    )
 }
 
 export default Footer
--- React/03.usestate/src/example/Ex11.jsx
+++ React/03.usestate/src/example/Ex11.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Footer from '../components/Ex11/Footer'
+import Content from '../components/Ex11/Content'
+import Header from '../components/Ex11/Header'
+import { ThemeContext } from '../context/Ex11ThemeContext'
+import '../style/ex11.css'
 
 const Ex11 = () => {
-  return (
-    <div>Ex11</div>
-  )
+    // state 생성 => isDark => 초기값(false)
+    const [isDark, setIsDark] = useState(false)
+
+    return (
+        // 만들어놓은 컨텍스트(ThemeContext)를 요소위에 덮어주기 => value속성 안에 => 스테이트, 스테이트 함수 넣기
+        <ThemeContext.Provider value={{isDark, setIsDark}}>
+        <div className='page'>
+            {/* 하위 컴포넌트 3개 불러오기 */}
+            <Header></Header>
+            <Content></Content>
+            <Footer></Footer>
+        </div>
+        </ThemeContext.Provider>
+    )
 }
 
 export default Ex11
