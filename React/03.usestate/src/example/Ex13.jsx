36c570aa253012317f5681ab8dab0f10e58eeb76
uni <moonrit001@gmail.com>
2025-07-14 10:22:14 +0900

    Ex13 시작

---
+++ React/03.usestate/src/example/Ex13.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react'
+
+const Ex13 = () => {
+  console.log("a. 함수초기화");
+
+  const [num, setNum] = useState(0)
+  
+  return (
+    <div></div>
+  )
+}
+
+export default Ex13
--- React/03.usestate/src/main.jsx
+++ React/03.usestate/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
-import App from './example/Ex12.jsx'
+import App from './example/Ex13.jsx'
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
