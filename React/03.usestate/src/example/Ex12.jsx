8c8528fb1181e23de77ee5de20a137101604b871
uni <moonrit001@gmail.com>
2025-07-14 10:10:13 +0900

    사정상 멈춤

--- React/03.usestate/src/example/Ex12.jsx
+++ React/03.usestate/src/example/Ex12.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { TodoContext } from '../context/EX12TodoContext'
+import List from '../components/Ex12/List'
+import AddItem from '../components/Ex12/AddItem'
 import '../style/ex12.css'
 
 const Ex12 = () => {
-  return (
-    <div>Ex12</div>
-  )
+    return (
+        <div>Ex12</div>
+    )
 }
 
 export default Ex12
