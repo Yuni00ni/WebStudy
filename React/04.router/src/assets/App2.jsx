6e46fd994fe36109f7046f55590e055c7f3457cb
uni <moonrit001@gmail.com>
2025-07-15 11:18:45 +0900

    뭔가 많이 함

--- React/04.router/src/assets/App2.jsx
+++ React/04.router/src/assets/App2.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+// 필요한 기능들 가져오기
+import { Route, Routes } from 'react-router-dom'
+import Main from './components/Main'
+import About from './components/About'
+import Product from './components/Product'
 
 const App2 = () => {
     return (
-        <div>App2</div>
+        /* React Router
+            사용자가 요청한 URL에 따라 해당 URL에 맞는 페이지를 보여주는 역할
+            1. React Router 설치 => 터미널 창에 npm i react-router-dom
+            2. main.jsx로 이동 후 => App컴포넌트를 BrowserRouter안에 넣기
+        
+        */
+        <div>
+            <h1>React Router</h1>
+            <Routes>
+                <Route path='/' element={<Main />}></Route>
+                <Route path='/about' element={<About />}></Route>
+                {/* 메인경로 뒤에 product라는 주소값이 붙게 되면은 => Product컴포넌트를 보여주겠다*/}
+                <Route path='/product' element={<Product />}></Route>
+            </Routes>
+        </div>
     )
 }
 
--- React/04.router/src/main.jsx
+++ React/04.router/src/main.jsx
@@ -6,6 +6,8 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
+  // BrowserRouter -> 라우터를 쓸 수 있게끔 연결
+  // App 컴포넌트 안에서 라우트 경로를 컴포넌트로 맵핑해서 동작되는 구조로 만들 예정
   <BrowserRouter>
     <App />
   </BrowserRouter>
