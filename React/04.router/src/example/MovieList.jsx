0168a0a5a4a42e2a563c88431543554283aca9b9
uni <moonrit001@gmail.com>
2025-07-16 10:25:13 +0900

    css 수정

--- React/04.router/src/example/MovieList.jsx
+++ React/04.router/src/example/MovieList.jsx
@@ -2,14 +2,25 @@ import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 import '../style/MovieList.css'
-import { API_KEYS } from '../config/config'
 
-// API키 변수 처리
+// API키 받아오기
+import { API_KEYS } from '../config/config'
 
 // URL 설정
 const URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEYS.API_KEY}`
 
 const MovieList = () => {
+    // state 생성
+    const [movie, setMovie] = useState([])
+
+    useEffect(() => {
+        // axios 요청을 통해서 콘솔창에 영화데이터를 출력
+        axios.get(URL).then((res) => {
+            console.log(res.data.results);
+            
+        })
+    }, [])
+
     return (
         <div>MovieList</div>
     )
