1d21c02f4baef9407732b7976ffd142bb7572a24
uni <moonrit001@gmail.com>
2025-07-16 11:11:50 +0900

    무비리스트 완

--- React/04.router/src/example/MovieList.jsx
+++ React/04.router/src/example/MovieList.jsx
@@ -17,12 +17,37 @@ const MovieList = () => {
         // axios 요청을 통해서 콘솔창에 영화데이터를 출력
         axios.get(URL).then((res) => {
             console.log(res.data.results);
-            
+            setMovie(res.data.results)
+
+        }).catch((e) => {
+            console.error('Error Data', e)
+
         })
     }, [])
 
     return (
-        <div>MovieList</div>
+        <div className='movie-list'>
+            <h1>Popular Movies</h1>
+            <Link to='/' className='home-link'>
+                <button>홈으로</button>
+            </Link>
+            <div className="movies">
+                {/* 응답받아온 data -> state안에 있는 상태
+                    map함수를 통해서 총 20개의 포스터를 제작
+                    `https://image.tmdb.org/t/p/w200`
+                */}
+                {movie.map((item) => (
+                    <div key= {item.id} className='movie'>
+                        <Link to='/movie'><img src={`https://image.tmdb.org/t/p/w200${item.poster_path}`} />
+                        </Link>
+                            <div className='movie-info'>
+                                <h2>{item.original_title}</h2>
+                                <p>Rating : {item.vote_average}</p>
+                            </div>
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 }
 
