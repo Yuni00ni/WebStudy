b0e41c1759353f8a27b98ccde9b6c00ae35549fe
uni <moonrit001@gmail.com>
2025-07-16 10:03:01 +0900

    ignore 추가

--- React/04.router/src/example/MovieList.jsx
+++ React/04.router/src/example/MovieList.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+import '../style/MovieList.css'
+import { API_KEYS } from '../config/config'
 
-const Movielist = () => {
+// API키 변수 처리
+
+// URL 설정
+const URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEYS.API_KEY}`
+
+const MovieList = () => {
     return (
-        <div>Movielist</div>
+        <div>MovieList</div>
     )
 }
 
-export default Movielist
+export default MovieList
