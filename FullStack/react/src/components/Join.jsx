31dbd4d754a6a9158a7eccbadc1617a90ee1b7c0
uni <moonrit001@gmail.com>
2025-07-18 16:11:33 +0900

    Ref

--- FullStack/react/src/components/Join.jsx
+++ FullStack/react/src/components/Join.jsx
@@ -5,6 +5,11 @@ import axios from 'axios'
 
 const Join = () => {
     // 화면에 떠 있는 DOM요소들 접근하기
+    const idRef = useRef()
+    const pwRef = useRef()
+    const nickRef = useRef()
+    const nav = useNavigate()
+
     return (
         <div>
             <h1>회원 가입 페이지입니다.</h1>
