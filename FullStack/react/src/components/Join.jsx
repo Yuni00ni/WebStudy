a984b2eb8ac47e193112f58fe71f55aa85019367
uni <moonrit001@gmail.com>
2025-07-18 15:58:45 +0900

    ignore setting

--- FullStack/react/src/components/Join.jsx
+++ FullStack/react/src/components/Join.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { useRef } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
 const Join = () => {
-  return (
-    <div>Join</div>
-  )
+    // 화면에 떠 있는 DOM요소들 접근하기
+    return (
+        <div>
+            <h1>회원 가입 페이지입니다.</h1>
+            ID : <input type="text" />
+            <br />
+            PW : <input type="password" />
+            <br />
+            NICK : <input type="text" />
+            <button>JOIN</button>
+            <button>초기화</button>
+        </div>
+    )
 }
 
 export default Join
